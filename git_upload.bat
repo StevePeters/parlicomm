5aca440475c66d5c8909c68561548cd00491fce0
Steve Peters <steve.peters@communities.gov.uk>
2020-12-31 17:13:08 +0000

    Delete git_upload.bat

--- git_upload.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add data/c_committees.csv
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/StevePeters/parlicomm.git
-git push -u origin main
